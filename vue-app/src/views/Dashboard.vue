aa4df07ab02d348f19c28987d158b81dc6722ca2
Mojjo202 <patrick.ongeche@stl-horizon.com>
2021-06-25 14:31:45 +0300

    first commit

---
+++ vue-app/src/views/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home col-8 mx-auto py-5 mt-5 ">
+    <div class="card">
+      <div class= "d-flex justify-content-center">
+    <h2>MS GRAPH</h2>
+    <hr>
+    <h2>Passport</h2>
+    <hr>
+    <h2>Socialite</h2>
+      </div>
+    <!-- <div class="card">
+      <div class="card-body" v-if="user">
+        <h3>Hello, {{ user.name }}</h3>
+        <span>{{ user.email }}</span>
+      </div>
+    </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import User from "../apis/User";
+
+export default {
+  data() {
+    return {
+      user: null
+    };
+  },
+
+  mounted() {
+    User.auth().then(response => {
+      this.user = response.data;
+    });
+  }
+};
+</script>
---
+++ vue-app/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home text-center py-5 mt-5">
+    <h1>LOGIN SCREENS</h1>
+    <hr>
+     <div class="home col-8 mx-auto py-5 mt-5 ">
+    <div class="card">
+      <div class= "d-flex justify-content-center">
+    <router-link class="nav-item nav-link" to="/msgraph">MS GRAPH</router-link>
+    <hr>
+    <router-link class="nav-item nav-link" to="/passport">PASSPORT</router-link>
+    <hr>
+    <router-link class="nav-item nav-link" to="/socialite">SOCIALITE</router-link>
+    <hr>
+    <router-link class="nav-item nav-link" to="/login">SANCTUM</router-link>
+      </div>
+    <!-- <div class="card">
+      <div class="card-body" v-if="user">
+        <h3>Hello, {{ user.name }}</h3>
+        <span>{{ user.email }}</span>
+      </div>
+    </div> -->
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
